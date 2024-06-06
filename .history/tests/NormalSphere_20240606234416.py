a64c19065becfd914cf363865d9e3f4f5154710f
Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
2024-06-07 03:01:28 +0800

    Squashed commit of the following:
    commit 7c3e116889b7db8d6175a063de2b2a3104702da5
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Fri Jun 7 01:51:53 2024 +0800
    
        chore:Setup the Experiment
    
    commit 8dd77d470815454039d9c32ed854603caa0d0316
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Fri Jun 7 00:36:19 2024 +0800
    
        chore: Visualize NormalSphere
    
    commit 29d3c78db4bc0dcf4ae1165a3563fb15a0a4696e
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Fri Jun 7 00:27:30 2024 +0800
    
        Update NormalSphere Test
    
    commit c11bbffd83a952dac798a8397ecf3c1aa7c444f9
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 23:20:38 2024 +0800
    
        chore: Update ScaML solver
    
    commit 2472e0259b67dd7f8b45c9af8e351b63f9d05bf6
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 19:32:34 2024 +0800
    
        chore: Updated batchwise MLP and fix equations bug
    
    commit 2ee38aae127562a3dff9e35a2b29d271582b0eb4
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 03:06:20 2024 +0800
    
        chore: Wandb Integration
    
    commit b669636f70077fb241d6b60c33c61eec1eeee50e
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 02:36:10 2024 +0800
    
        chore: Define Adam-LBFGS optimizer
    
    commit 5741c2928c90b99de16404d6b3024f4576c9011a
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 01:55:36 2024 +0800
    
        chore: Remove unused files and define FNN model
    
    commit 4bb15ab90a8c7688feef6bdafd832f8c73af14fc
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 01:31:38 2024 +0800
    
        chore: Explicit Solution Example
    
    commit ecab486f0417c4f8e8bf4574570d00efe81e985b
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Thu Jun 6 01:01:39 2024 +0800
    
        chore: Update Equation class for PDEs based on deepxde framework
    
    commit c1f3de873aa992dc9a6ed2db54258a514ed7ed3a
    Author: Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
    Date:   Wed Jun 5 03:05:15 2024 +0800
    
        chore: Update dataset and environment configuration

---
+++ .history/tests/NormalSphere_20240606234416.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class NormalShperes(object):
+    '''Normal spheres in high dimensions'''
+    def __init__(self, equation, solver1,solver2,solver3):
+        #initialize the normal spheres
+        #solver1 for PINN
+        #solver2 for MLP
+        #solver3 for ScaML
+        self.equation=equation
+        self.dim=equation.n_input-1
+        self.solver1=solver1
+        self.solver2=solver2
+        self.solver3=solver3
+        self.t0=equation.t0
+        self.T=equation.T
+        self.radius=np.sqrt(self.dim*(self.T-self.t0)**2)
+    def test(self,n_samples=10,x_grid_num=100,t_grid_num=10):
+        #compare solvers on different distances on the sphere
+        x_grid=np.linspace(0,self.radius,x_grid_num)
+        t_grid=np.linspace(self.t0,self.T,t_grid_num)
+        x_mesh,t_mesh=np.meshgrid(x_grid,t_grid)
+        errors=np.zeros_like(x_mesh)
+        for i in range(x_mesh.shape[0]):
+            for j in range(x_mesh.shape[1]):
+                x_values=np.random.normal(0,1,(n_samples,self.dim))
+                x_values/=np.linalg.norm()
