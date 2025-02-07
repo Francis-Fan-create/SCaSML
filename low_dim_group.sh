341bfcb0ca53a777f4f564d5f58a37493121052b
Francis-Fan-Create <2200010816@stu.pku.edu.cn>
2025-02-08 07:08:22 +0800

    low dim group complete

--- low_dim_group.sh
+++ low_dim_group.sh
@@ -3,45 +3,29 @@
 #device: vGPU 32G
 
 # Group 1
-echo "Running Group 1 - Task 1..."
+echo "Running Group 1"
 python results/Grad_Dependent_Nonlinear/20d/experiment_run.py &
-wait
-echo "Group 1 completed."
-
-echo "Running Group 1 - Task 2..."
 python results/Linear_HJB/20d/experiment_run.py &
 wait
 echo "Group 1 completed."
 
 # Group 2
-echo "Running Group 4 - Task 1..."
+echo "Running Group 2"
 python results/Grad_Dependent_Nonlinear/40d/experiment_run.py &
-wait
-echo "Group 2 completed."
-
-echo "Running Group 4 - Task 2..."
 python results_full_history/Grad_Dependent_Nonlinear/20d/experiment_run.py &
 wait
 echo "Group 2 completed."
 
 # Group 3
-echo "Running Group 3..."
+echo "Running Group 3"
 python results_full_history/Grad_Dependent_Nonlinear/40d/experiment_run.py &
-wait
-echo "Group 3 completed."
-
-echo "Running Group 3..."
 python results_full_history/Linear_HJB/20d/experiment_run.py &
 wait
 echo "Group 3 completed."
 
 # Group 4
-echo "Running Group 4 - Task 1..."
+echo "Running Group 4"
 python results/Linear_HJB/40d/experiment_run.py &
-wait
-echo "Group 4 - Task 1 completed."
-
-echo "Running Group 4 - Task 2..."
 python results_full_history/Linear_HJB/40d/experiment_run.py &
 wait
 echo "Group 4 - Task 2 completed."
