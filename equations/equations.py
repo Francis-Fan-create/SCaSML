2f6046b41b134bbc42b6052a5d4d5015c69c49fc
Francis-Fan-Create <2200010816@stu.pku.edu.cn>
2025-02-26 18:40:35 +0800

    low dim diffusion reaction

--- equations/equations.py
+++ equations/equations.py
@@ -698,7 +698,7 @@ class Linear_HJB(Equation):
         - x_t (ndarray): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
         
         Returns:
-        - result (ndarray): An arrary of shape (batch_size, ), representing the exact solution.
+        - result (ndarray): An arrary of shape (batch_size, n_output), representing the exact solution.
         '''
         t = x_t[:, -1]
         x = x_t[:, :-1]
@@ -749,6 +749,207 @@ class Linear_HJB(Equation):
                                 num_domain=num_domain, # Number of domain points.
                                 num_boundary=2360, # Number of boundary points.
                                 num_initial=0,  # Number of initial points.
+                                solution=self.exact_solution   # Incorporates exact solution for error metrics.
+                            )
+        return data
+
+class Diffusion_Reaction(Equation):
+    '''
+    Diffusion Reaction equation.
+    '''
+    def __init__(self, n_input, n_output=1):
+        '''
+        Initializes the PDE example with specified input and output dimensions.
+        
+        Parameters:
+        - n_input (int): The dimension of the input space, including the time dimension.
+        - n_output (int): The dimension of the output space. Defaults to 1.
+        '''
+        super().__init__(n_input, n_output)
+        self.uncertainty = 2e-3
+        self.norm_estimation = jnp.sqrt(jnp.e)
+    
+    def PDE_loss(self, x_t,u):
+        '''
+        Calculates the PDE loss for given inputs.
+        
+        Parameters:
+        - x_t (tensor): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
+        - u (tensor): Output tensor of shape (batch_size, n_output), representing the solution of the PDE.
+        
+        Returns:
+        - residual (tensor): The residual of the PDE of shape (batch_size, n_output).
+        '''
+        du_t = dde.grad.jacobian(u,x_t,i=0,j=self.n_input-1)[0] # Computes the time derivative of u.
+        laplacian=0
+        MC = int(self.n_input/4)
+        dim=self.n_input-1
+        # randomly choose MC dims to compute hessian and div
+        idx_list = np.random.choice(self.n_input-1, MC, replace=False)
+        for k in idx_list: # Accumulates laplacian and divergence over spatial dimensions.
+            laplacian +=dde.grad.hessian(u, x_t, i=k, j=k)[0] # Computes the laplacian of z.
+        laplacian *= dim/MC
+        x_1 = x_t[:, 0]
+        x_2 = x_t[:, 1]
+        t = x_t[:, -1]
+        f_val = (jnp.pi**2-2)*jnp.sin(jnp.pi*x_1/2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-t) - 4* (jnp.sin(jnp.pi*x_1/2)**2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-2*t)
+        f_val = f_val[:,jnp.newaxis]
+        residual=du_t +laplacian+u[0]**2+f_val # Computes the residual of the PDE.
+        return residual 
+    
+    @partial(jit,static_argnames=["self"])
+    def initial_constraint(self, x_t):
+        '''
+        Defines the initial constraint for the PDE.
+        
+        Parameters:
+        - x_t (ndarray): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
+        
+        Returns:
+        - result (ndarray): A 2D tensor of shape (batch_size, 1), representing the initial constraint.
+        '''
+        t = x_t[:, -1]
+        x_1 = x_t[:, 0]
+        x_2 = x_t[:, 1]
+        result = -jnp.sin(jnp.pi*x_1/2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-t)
+        return result[:,jnp.newaxis]
+
+    @partial(jit,static_argnames=["self"])
+    def terminal_constraint(self, x_t):
+        '''
+        Defines the terminal constraint for the PDE.
+        
+        Parameters:
+        - x_t (ndarray): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
+        
+        Returns:
+        - result (ndarray): A 2D tensor of shape (batch_size, 1), representing the terminal constraint.
+        '''
+        t = x_t[:, -1]
+        x_1 = x_t[:, 0]
+        x_2 = x_t[:, 1]
+        result = -jnp.sin(jnp.pi*x_1/2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-t)
+        return result[:,jnp.newaxis]
+    
+    @partial(jit,static_argnames=["self"])
+    def Dirichlet_boundary_constraint(self, x_t):
+        '''
+        Defines the Dirichlet boundary constraint for the PDE.
+        
+        Parameters:
+        - x_t (ndarray): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
+        
+        Returns:
+        - result (ndarray): A 2D tensor of shape (batch_size, 1), representing the Dirichlet boundary constraint.
+        '''
+        t = x_t[:, -1]
+        x_1 = x_t[:, 0]
+        x_2 = x_t[:, 1]
+        result = -jnp.sin(jnp.pi*x_1/2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-t)
+        return result[:,jnp.newaxis]
+
+    def mu(self, x_t=0):
+        '''
+        Returns the drift coefficient of the PDE. Here, it's a constant value.
+        
+        Parameters:
+        - x_t (int, optional): Not used in this implementation.
+        
+        Returns:
+        - (float): The drift coefficient.
+        '''
+        return 0
+    
+    def sigma(self, x_t=0):
+        '''
+        Returns the diffusion coefficient of the PDE. Here, it's a constant value.
+        
+        Parameters:
+        - x_t (int, optional): Not used in this implementation.
+        
+        Returns:
+        - (float): The diffusion coefficient.
+        '''
+        return jnp.sqrt(2)
+    
+    @partial(jit,static_argnames=["self"])    
+    def f(self, x_t,u,z):
+        '''
+        Defines the generator term for the PDE.
+        
+        Parameters:
+        - x_t (ndarray): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
+        - u (ndarray): Output tensor of shape (batch_size, n_output), representing the solution of the PDE.
+        - z (ndarray): Tensor of shape (batch_size, n_input-1), representing gradients.
+        
+        Returns:
+        - result (ndarray): A 2D array of shape (batch_size, n_output), representing the generator term.
+        '''
+        x_1 = x_t[:, 0]
+        x_2 = x_t[:, 1]
+        t = x_t[:, -1]
+        f_val = (jnp.pi**2-2)*jnp.sin(jnp.pi*x_1/2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-t) - 4* (jnp.sin(jnp.pi*x_1/2)**2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-2*t)
+        return f_val[:,jnp.newaxis]+u**2
+    
+    @partial(jit,static_argnames=["self"])    
+    def exact_solution(self, x_t):
+        '''
+        Computes the exact solution of the PDE for given inputs.
+        
+        Parameters:
+        - x_t (ndarray): Input tensor of shape (batch_size, n_input), where n_input includes the time dimension.
+        
+        Returns:
+        - result (ndarray): An arrary of shape (batch_size, n_ouput), representing the exact solution.
+        '''
+        t = x_t[:, -1]
+        x_1 = x_t[:, 0]
+        x_2 = x_t[:, 1]
+        result = -jnp.sin(jnp.pi*x_1/2)*jnp.cos(jnp.pi*x_2/2)*jnp.exp(-t)
+        return result[:,jnp.newaxis]
+    
+    def geometry(self,t0=0,T=0.5):
+        '''
+        Defines the geometry of the domain for the PDE.
+        
+        Parameters:
+        - t0 (float): Initial time.
+        - T (float): Terminal time.
+        
+        Returns:
+        - geom (dde.geometry.GeometryXTime): A GeometryXTime object representing the domain.
+        '''
+        self.t0=t0
+        self.T=T
+        spacedomain = dde.geometry.Hypercube([-0.5]*(self.n_input-1), [0.5]*(self.n_input-1)) # Defines the spatial domain, for train
+        timedomain = dde.geometry.TimeDomain(t0, T) # Defines the time domain.
+        geom = dde.geometry.GeometryXTime(spacedomain, timedomain) # Combines spatial and time domains.
+        self.geomx=spacedomain
+        self.geomt=timedomain
+        return geom
+    
+    def generate_data(self, num_domain=2500):
+        '''
+        Generates data for training the PDE model.
+        
+        Parameters:
+        - num_domain (int): Number of points to sample in the domain.
+        
+        Returns:
+        - data (dde.data.TimePDE): A TimePDE object containing the training data.
+        '''
+        geom=self.geometry() # Defines the geometry of the domain.
+        # self.terminal_condition() # Generates terminal condition.
+        self.Dirichlet_boundary_condition() # Generates Dirichlet boundary condition.
+        self.initial_condition() # Generate initial condition
+        # self.data_loss() # Generates data loss. 
+        data = dde.data.TimePDE(
+                                geom, # Geometry of the domain.
+                                self.PDE_loss, # PDE loss function.
+                                [self.ic, self.D_bc], # Additional conditions.
+                                num_domain=num_domain, # Number of domain points.
+                                num_boundary=100, # Number of boundary points.
+                                num_initial=160,  # Number of initial points.
                                 solution=self.exact_solution   # Incorporates exact solution for error metrics.
                             )
         return data
---
+++ results/Diffusion_Reaction/20d/experiment_run.py
@@ -0,0 +1,93 @@
+# We will use this file to run the experiment. We will train the model and then test it on the NormalSphere test. We will also profile the test to see the time taken by each solver to solve the equation. We will save the profiler results_full_history and upload them to wandb.
+import sys
+import os
+#add the parent directory to the path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+
+
+# import the required libraries
+from equations.equations import Diffusion_Reaction
+from optimizers.Adam import Adam 
+# L_inf has been removed
+from tests.SimpleUniform import SimpleUniform
+from tests.ConvergenceRate import ConvergenceRate
+from tests.InferenceScaling import InferenceScaling
+from solvers.MLP import MLP
+from solvers.ScaSML import ScaSML
+import numpy as np
+import torch
+import wandb
+import deepxde as dde
+import jax
+
+
+#fix random seed for dde
+dde.config.set_random_seed(1234)
+#use jax backend
+dde.backend.set_default_backend('jax')
+#set default float to float16
+dde.config.set_default_float("float16")
+# fix random seed for jax
+jax.random.PRNGKey(0)
+# device configuration
+device = jax.default_backend()
+print(device)
+if device == 'gpu':
+    # get PINNU name
+    gpu_name = jax.devices()[0].device_kind
+
+#initialize wandb
+wandb.init(project="Diffusion_Reaction", notes="20 d", tags=["Adam training","L_inf_training"],mode="disabled") #debug mode
+# wandb.init(project="Diffusion_Reaction", notes="20 d", tags=["Adam training","L_inf_training"]) #working mode
+wandb.config.update({"device": device}) # record device type
+
+#initialize the equation
+equation=Diffusion_Reaction(n_input=21,n_output=1)
+#check if trained model is already saved
+if os.path.exists(r"results/Diffusion_Reaction/80d/model.ckpt-?"):
+    '''To Do: Retrain the model with new data points& Try new methods to reduce errors'''
+    #load the model
+    net=dde.maps.jax.FNN([21]+[50]*5+[1], "tanh", "Glorot normal")
+    terminal_transform = equation.terminal_transform
+    net.apply_output_transform(terminal_transform)
+    data = equation.generate_data()
+    model = dde.Model(data,net)
+    model.restore(r"results/Diffusion_Reaction/20d/model.ckpt-?",verbose=1)
+    # set is_train to False
+    is_train = False
+else:
+    #initialize the FNN
+    #same layer width
+    net1=dde.maps.jax.FNN([21]+[50]*5+[1], "tanh", "Glorot normal")
+    net2=dde.maps.jax.FNN([21]+[50]*5+[1], "tanh", "Glorot normal")
+    net3=dde.maps.jax.FNN([21]+[50]*5+[1], "tanh", "Glorot normal")    
+    data1 = equation.generate_data()
+    data2 = equation.generate_data()
+    data3 = equation.generate_data()
+    model1 = dde.Model(data1,net1)
+    model2 = dde.Model(data2,net2)
+    model3 = dde.Model(data3,net3)
+    is_train = True
+
+
+#initialize the normal sphere test
+solver1_1= model1 #PINN network
+solver1_2 = model2 #PINN network
+solver1_3 = model3 # PINN network
+solver2=MLP(equation=equation) #Multilevel Picard object
+solver3_1=ScaSML(equation=equation,PINN=solver1_1) #ScaSML object
+solver3_2=ScaSML(equation=equation,PINN=solver1_2) #ScaSML object
+solver3_3=ScaSML(equation=equation,PINN=solver1_3) #ScaSML object
+
+
+#run the test for SimpleUniform
+test2=SimpleUniform(equation,solver1_1,solver2,solver3_1,is_train)
+test2.test(r"results/Diffusion_Reaction/20d")
+#run the test for ConvergenceRate
+test3=ConvergenceRate(equation,solver1_2,solver2,solver3_2, is_train)
+test3.test(r"results/Diffusion_Reaction/20d")
+
+
+#finish wandb
+wandb.finish()
+
